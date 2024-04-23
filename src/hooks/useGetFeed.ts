3d35d25bff7365dc187bb1eeb3437197c68d2510
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-04-23 17:08:33 -0300

    Refactor useGetFeed and useGetRecommendations hooks to use react-query and remove unused state variables

--- src/hooks/useGetFeed.ts
+++ src/hooks/useGetFeed.ts
@@ -1,13 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { setLength, updateFeed } from "../redux/user";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { useGetPosts } from "../service/query";
 
 export const useGetFeed = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [enabled, setEnabled] = useState<boolean>(false);
-
   const posts = useAppSelector((state) => state.user.feed);
   const query = useAppSelector((state) => state.user.query);
 
@@ -16,21 +12,18 @@ export const useGetFeed = () => {
   const {
     data: Posts,
     refetch: refetchPosts,
+    error,
     isLoading
-  } = useGetPosts(query, enabled);
+  } = useGetPosts(query, true);
 
   useEffect(() => {
-    setLoading(true);
-    setError(false);
-    setEnabled(true);
     refetchPosts();
     if (Posts && !isLoading) {
       const updatedPosts = Array.from(new Set([...posts, ...Posts]));
       dispatch(updateFeed(updatedPosts));
       dispatch(setLength(updatedPosts.length));
-      setLoading(false);
     }
   }, [Posts, query]);
 
-  return { posts, loading, error };
+  return { posts, isLoading, error };
 };
--- src/hooks/useGetRecommendations.ts
+++ src/hooks/useGetRecommendations.ts
@@ -8,39 +8,39 @@ interface UseGetRecommendationsProps {
 
 export const useGetRecommendations = ({ page }: UseGetRecommendationsProps) => {
   const [users, setUsers] = useState<Author[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [hasMore, setHasMore] = useState(true); // Nuevo estado para verificar si hay más elementos
   const [enabled, setEnabled] = useState<boolean>(false);
 
   const {
     data: recommendedUsers,
     refetch: refetchRecommendedUsers,
+    error,
     isLoading,
   } = useGetRecommendedUsers(10, page, enabled);
-
+  
   useEffect(() => {
     if (page !== undefined && hasMore) {
-      setLoading(true);
-      setError(false);
       setEnabled(true);
       refetchRecommendedUsers();
-      if (recommendedUsers && !isLoading) {
-        if (recommendedUsers.length === 0) {
-          setHasMore(false);
-        } else {
-          setUsers((prev) => {
-            const uniqueIds = new Set(prev.map((user) => user.id));
-            const filteredUsers = recommendedUsers.filter(
-              (user: Author) => !uniqueIds.has(user.id)
-            );
-            return [...prev, ...filteredUsers];
-          });
-          setLoading(false);
-        }
-      }
     }
   }, [page, hasMore]);
 
-  return { users, loading, error };
+  useEffect(() => {
+    if (recommendedUsers && !isLoading) {
+      if (recommendedUsers.length === 0) {
+        setHasMore(false);
+      } else {
+        setUsers((prev) => {
+          const uniqueIds = new Set(prev.map((user) => user.id));
+          const filteredUsers = recommendedUsers.filter(
+            (user: Author) => !uniqueIds.has(user.id)
+          );
+          return [...prev, ...filteredUsers];
+        });
+      }
+    }
+  }, [recommendedUsers]);
+
+
+  return { users, isLoading, error };
 };
